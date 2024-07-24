b66a380b89354ff673399487b8c8bd5ec83de025
Ozbayraktaryusuf <102061148+Ozbayraktaryusuf@users.noreply.github.com>
2024-07-24 11:12:54 +0300

    Add files via upload

---
+++ managementService/src/main/java/com/atmosware/belatrix/managmentService/ManagmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.atmosware.belatrix.managmentService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagmentServiceApplication.class, args);
+	}
+
+}
---
+++ managementService/src/test/java/com/atmosware/belatrix/managmentService/ManagmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atmosware.belatrix.managmentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
